d8d7a2ebcb9dc73c3f6bf6cf5b71595afbc59269
Chris L <chris.luginbuhl@gmail.com>
2025-07-03 07:19:59 -0700

    Update Temp_monitor.ino
    Use Particle.variable() rather than .publish()

--- Temp_monitor.ino
+++ Temp_monitor.ino
@@ -1,43 +1,37 @@
-// Temperature sensing for boat using Particle Boron and Webhooks
-// Notes from Nov 2022:
 
-// Not yet version controled on Github as falcor_iob.
-
+// Temperature sensing for using Particle Boron and an exposed variable
 #include <Adafruit_BME680.h>
-
 Adafruit_BME680 bme; // I2C
-
-const unsigned long PUBLISH_PERIOD_MS = 1800000;
+const unsigned long PUBLISH_PERIOD_MS = 1800000; // 30 minutes
 const char *FIELD_SEPARATOR = "|||";
-const char *EVENT_NAME = "tempSensor";
-const char *ALARM_NAME = "tempAlarm";
 const float LOW_TEMP_THRESHOLD = 2.0;
-
 bool sensorReady = false;
 bool alarmStatus = false;
 unsigned long lastPublish = 0;
-char buf[256];
+char sensorData[256]; // This will hold our sensor reading
 int publishCount = 0;
-
 void setup() {
     Serial.begin(9600);
-
     sensorReady = bme.begin();
+    // Expose the sensor data as a Particle variable
+    Particle.variable("sensorData", sensorData);
+    // Initialize with default value
+    strcpy(sensorData, "0.00|||0.00|||0.0");
 }
-
-
 void loop() {
-    if ((publishCount < 10) && (millis() - lastPublish >= (int)PUBLISH_PERIOD_MS/30 && sensorReady) || (millis() - lastPublish >= PUBLISH_PERIOD_MS && sensorReady)) {
+    if ((publishCount < 10) && (millis() - lastPublish >= (int)PUBLISH_PERIOD_MS/30 && sensorReady) || 
+        (millis() - lastPublish >= PUBLISH_PERIOD_MS && sensorReady)) {
         lastPublish = millis();
-
         float temp = bme.readTemperature(); // degrees C
         float pressure = bme.readPressure() / 100.0; // hPa
         float humidity = bme.readHumidity(); // %
-
-        snprintf(buf, sizeof(buf), "%.02f%s%.02f%s%.01f", temp, FIELD_SEPARATOR, pressure, FIELD_SEPARATOR, humidity);
-        Particle.publish(EVENT_NAME, buf, PRIVATE);
+        // Update the variable (this is what we'll poll)
+        snprintf(sensorData, sizeof(sensorData), "%.02f%s%.02f%s%.01f", 
+                temp, FIELD_SEPARATOR, pressure, FIELD_SEPARATOR, humidity);
+        // Optional: still publish events if you want
+        // Particle.publish("tempSensor", sensorData, PRIVATE);
         if ((temp < LOW_TEMP_THRESHOLD) && !alarmStatus) {
-            Particle.publish(ALARM_NAME, "Boat low temp alarm");
+            // Particle.publish("tempAlarm", "Boat low temp alarm");
             alarmStatus = true;
         } else if (temp > LOW_TEMP_THRESHOLD + 1.0) {
             alarmStatus = false;
